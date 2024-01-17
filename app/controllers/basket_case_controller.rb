96d32439aa3fd84224658cd2e1492be3173d9f2f
WillNigel23 <wcdejesus@up.edu.ph>
2024-01-17 16:34:25 +0800

    test/mid-2 Sort asc/desc

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,16 +1,25 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :set_order
 
   def index; end
 
   def order_by_fruit
-    @list_items = @list_items.sort
+    if @order == 'asc'
+      @list_items = @list_items.sort_by(&:first)
+    else
+      @list_items = @list_items.sort_by(&:first).reverse
+    end
     render 'basket_case/index'
   end
 
   def order_by_amount
-    @list_items = @list_items.sort_by(&:last)
+    if @order == 'asc'
+      @list_items = @list_items.sort_by(&:last)
+    else
+      @list_items = @list_items.sort_by(&:last).reverse
+    end
     render 'basket_case/index'
   end
 
@@ -26,5 +35,10 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def set_order
+      valid_orders = %w[asc desc]
+      @order = valid_orders.include?(params[:order]) ? params[:order] : 'asc'
     end
 end
